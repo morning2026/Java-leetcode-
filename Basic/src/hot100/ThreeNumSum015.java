27d50e129a7c02c7292f88e8a909c4ef4656cd0f
morning2026 <morning2026@users.noreply.github.com>
2026-04-12 00:36:26 +0800

    双指针

---
+++ Basic/src/hot100/ThreeNumSum015.java
@@ -0,0 +1,46 @@
+package hot100;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ThreeNumSum015 {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (nums == null || nums.length < 3) return res;
+        Arrays.sort(nums);
+        int left, right;
+        for (int i = 0; i <= nums.length - 1; i++) {
+            if (nums[i] > 0) break;
+            if (i>0 && nums[i] == nums[i - 1]) continue;//i为0的时候会溢出，问i+1会漏解（左指针）
+            left = i+1;
+            right = nums.length - 1;
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                if (sum == 0) {
+                    res.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    //break; 漏解
+                    while (left < right && nums[left] == nums[left + 1]) left++;
+                    while (left < right && nums[right] == nums[right - 1]) right--;
+                    //找到一个三元组之后注意去重
+                    left++;
+                    right--;//指针要动
+                }
+                else if (sum < 0) {
+                    //while (left < right && nums[left] == nums[left + 1]) left++; 没必要，反而多套一层
+                    left++;
+                }
+                else {
+                    //while (left < right && nums[right] == nums[right - 1]) right--;
+                    right--;
+                }
+            }
+        }
+        return res;
+    }
+    public static void main(String[] args) {
+        ThreeNumSum015 threeNumSum015 = new ThreeNumSum015();
+        List<List<Integer>> res = threeNumSum015.threeSum(new int[]{1,2,0,1,0,0,0,0});
+        System.out.println(res);
+    }
+}
--- Basic/src/其他/removeval027.java
+++ Basic/src/其他/removeval027.java
@@ -1,4 +1,13 @@
 package 其他;
 
 public class removeval027 {
+    public int removeElement(int[] nums, int val) {
+        int cnt = 0;
+        for(int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[cnt++] = nums[i];
+            }
+        }
+        return cnt;
+    }
 }
