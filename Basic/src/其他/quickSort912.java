b8a33554b29e288d300544c61d85448badc701a6
morning2026 <morning2026@users.noreply.github.com>
2026-05-11 23:29:37 +0800

    第二轮-22

--- Basic/src/其他/quickSort912.java
+++ Basic/src/其他/quickSort912.java
@@ -3,9 +3,28 @@ import java.util.Random;
 public class quickSort912 {
     Random rand = new Random();
     public int[] sortArray(int[] nums) {
-        return sort(nums,0,nums.length-1);
+        sort(nums,0,nums.length-1);
+        return nums;
     }
-    private int[] sort(int[] nums,int l,int r){
-        int cur = l+
+    private void sort(int[] nums,int l,int r){
+        if(l>=r)
+            return;
+        int cur = l+ rand.nextInt(r-l+1); //生成0-n的随机数，左闭右开
+        swap(nums,cur,r);
+        int lnow = l;
+        for(int i=l;i<r;i++){
+            if(nums[i]<nums[r]){
+                swap(nums,lnow,i);
+                lnow++;
+            }
+        }
+        swap(nums,lnow,r);
+        sort(nums,l,lnow-1);
+        sort(nums,lnow+1,r);
+    }
+    private void swap(int[] nums,int i,int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
